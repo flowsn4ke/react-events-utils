91b8433718e8d08137402fc5112abfdee0503bf1
Theo Louvel <louveltheophile@gmail.com>
2022-01-02 17:32:20 +0100

    forgot to export...

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import { on, once, off, trigger } from "./events"
 import useDocumentListener from "./useDocumentListener"
+import useDocumentListenerOnce from "./useDocumentListenerOnce"
+import useEvent from "./useEvent"
+import useEventOnce from "./useEventOnce"
 
 export {
   on,
   once,
   off,
   trigger,
-  useDocumentListener
+  useDocumentListener,
+  useDocumentListenerOnce,
+  useEvent,
+  useEventOnce
 }
