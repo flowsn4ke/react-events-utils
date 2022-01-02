2da30a9d8bf3169e76c9acc856f4ea438da4b4c6
Theo Louvel <louveltheophile@gmail.com>
2022-01-02 18:07:51 +0100

    handle refs

--- src/hooks/useEvent.js
+++ src/hooks/useEvent.js
@@ -3,7 +3,10 @@ import { off, on } from "../events"
 
 export default function useEvent(target, event, handler) {
   useEffect(() => {
+    target = target && 'current' in target ? target.current : target
+    if (!target) return
+
     on(target, event, handler)
     return () => off(target, event, handler)
-  }, [event])
+  }, [event, target])
 }
